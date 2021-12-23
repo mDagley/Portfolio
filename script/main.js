31cba94da9533c34d28a071c0dbaf8ae26cdbfd4
Melissa Dagley <melissadagley@gmail.com>
2021-12-22 20:32:59 -0600

    Populate Modal Data

--- script/main.js
+++ script/main.js
@@ -31,12 +31,36 @@ closeButton.addEventListener("click", function () {
     modalOverlay.style.display = "none";
 })
 
-function openModal() {
+function openModal(index) {
     let modal = document.getElementById("projectModal");
     modal.style.display = "block";
 
     let modalOverlay = document.getElementById("modalOverlay");
     modalOverlay.style.display = "block";
+
+    let modalTitle = document.getElementById("modalTitle");
+    modalTitle.innerHTML = projects[index].title;
+
+    let modalDescription = document.getElementById("modalDescription");
+    modalDescription.innerHTML = projects[index].description;
+
+    let modalDate = document.getElementById("modalDate");
+    modalDate.innerHTML = projects[index].date;
+
+    let modalTechnology = document.getElementById("modalTechnology");
+    modalTechnology.innerHTML = projects[index].technology;
+
+    let modalPurpose = document.getElementById("modalPurpose");
+    modalPurpose.innerHTML = projects[index].purpose;
+
+    let modalOutcomes = document.getElementById("modalOutcomes");
+    modalOutcomes.innerHTML = projects[index].outcomes;
+
+    let liveLink = document.getElementById("modalLiveLink");
+    liveLink.setAttribute("href", projects[index].url);
+
+    let gitHubLink = document.getElementById("modalGitHubLink");
+    gitHubLink.setAttribute("href", projects[index].github);
 }
 
 for (i = 0; i < projects.length; i++) {
@@ -45,8 +69,8 @@ for (i = 0; i < projects.length; i++) {
         <div class='project-card'>
             <div class='project-image'>
                 <img src='/assets/` + projects[i].thumbnail + `'>
-                <div class='project-overlay' onclick="openModal()">
-                    <button type='button' class='view-more-button' onclick="openModal()">VIEW MORE</button>
+                <div class='project-overlay' onclick="openModal(` + i + `)">
+                    <button type='button' class='view-more-button' onclick="openModal(` + i + `)">VIEW MORE</button>
                 </div>
             </div>
             <h3 class='project-title'>` + projects[i].title + `</h3>
