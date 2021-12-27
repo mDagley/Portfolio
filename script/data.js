e0c0674c57d07961f84e8a4b97a4ca1858c7c0a5
Melissa Dagley <melissadagley@gmail.com>
2021-12-27 11:39:26 -0600

    Remove Test Project Data

--- script/data.js
+++ script/data.js
@@ -13,27 +13,9 @@ var projects =
             "photos":
                 [
                     {
-                        "url": "panda-ipsum-thumbnail.png",
+                        "url": "panda-ipsum-thumbnail.PNG",
                         "alt": "",
                         "title": ""
-                    }
-                ]
-        },
-        {
-            "title": "Example Title 2",
-            "description": "This is another example description",
-            "thumbnail": "panda-2.png",
-            "date": "December 25, 2021",
-            "technology": "HTML, CSS, Javascript",
-            "purpose": "To test my projects data object",
-            "outcomes": "Able to loop through a javascript object to build out the list of projects and dynamically populate the modal window",
-            "url": "https://mdagley.github.io/Portfolio/",
-            "github": "https://github.com/mDagley/Portfolio",
-            "photos":
-                [
-                    {
-                        "url": "",
-                        "alt": "", "title": ""
                     }
                 ]
         }
