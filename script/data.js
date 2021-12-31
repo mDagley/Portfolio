7bb6640c19bb92864e41b1f686123dd32011c706
Melissa Dagley <melissadagley@gmail.com>
2021-12-30 21:05:34 -0600

    Add Project Images and Data

--- script/data.js
+++ script/data.js
@@ -13,10 +13,28 @@ var projects =
             "photos":
                 [
                     {
-                        "url": "panda-ipsum-thumbnail-2.png",
-                        "alt": "",
-                        "title": "",
-                        "caption": "This is a test caption"
+                        "url": "/panda-ipsum/home.png",
+                        "alt": "Screenshot of the default view of the panda ipsum generator",
+                        "title": "The default view of the panda ipsum generator",
+                        "caption": "The default view of the panda ipsum generator"
+                    },
+                    {
+                        "url": "/panda-ipsum/tags.png",
+                        "alt": "Screenshot of the panda ipsum generator with the html tags shown",
+                        "title": "The panda ipsum generator with the HTML tags set to show",
+                        "caption": "The panda ipsum generator with the HTML tags set to show"
+                    },
+                    {
+                        "url": "/panda-ipsum/paragraphs.png",
+                        "alt": "Screenshot of the panda ipsum generator with the number of paragraphs changed",
+                        "title": "The panda ipsum generator with the number of paragraphs changed",
+                        "caption": "The panda ipsum generator with the number of paragraphs changed"
+                    },
+                    {
+                        "url": "/panda-ipsum/copy.png",
+                        "alt": "Screenshot of the panda ipsum generator after clicking the copy button",
+                        "title": "Screenshot of the panda ipsum generator after clicking the copy button",
+                        "caption": "Screenshot of the panda ipsum generator after clicking the copy button"
                     }
                 ],
             "type": "personal"
@@ -34,10 +52,46 @@ var projects =
             "photos":
                 [
                     {
-                        "url": "conrex-thumbnail.png",
-                        "alt": "",
-                        "title": "",
-                        "caption": "This is a test caption"
+                        "url": "conrex/home-map.png",
+                        "alt": "Screenshot of the Conrex custom site home page map section",
+                        "title": "Screenshot of the Conrex custom site home page map section",
+                        "caption": "This section is created using a custom svg and data contained in a json object that populates the page as different states are selected."
+                    },
+                    {
+                        "url": "conrex/about-map.png",
+                        "alt": "Screenshot of the Conrex custom site about page map section",
+                        "title": "Screenshot of the Conrex custom site about page map section",
+                        "caption": "This section is created using a custom svg and data contained in a json object that populates the page as different states are selected."
+                    },
+                    {
+                        "url": "conrex/customer-stories.png",
+                        "alt": "Screenshot of the Conrex customer testimonial section",
+                        "title": "Screenshot of the Conrex customer testimonial section",
+                        "caption": "This is a custom widget that I built for Conrex. This widget is used on a few different pages on the site."
+                    },
+                    {
+                        "url": "conrex/find-your-home-map.png",
+                        "alt": "Screenshot of the Conrex custom site find your home page map section",
+                        "title": "Screenshot of the Conrex custom site find your home page map section",
+                        "caption": "This section is created using a custom svg and data contained in a json object that populates the page as different states are selected."
+                    },
+                    {
+                        "url": "conrex/contact-widget.png",
+                        "alt": "Screenshot of the Conrex custom site contact page widget section",
+                        "title": "Screenshot of the Conrex custom site contact page widget section",
+                        "caption": "This is another custom widget that was built for Conrex. This widget uses the same JSON data object as the map sections on the site."
+                    },
+                    {
+                        "url": "conrex/custom-widget.png",
+                        "alt": "Screenshot of the Conrex custom site future residents page custom widget",
+                        "title": "Screenshot of the Conrex custom site future residents page custom widget",
+                        "caption": "This is a custom widget that I built for Conrex, it is used multiple times on the Future Residents page."
+                    },
+                    {
+                        "url": "conrex/wufoo-form.png",
+                        "alt": "Screenshot of the Conrex custom site Sell Us Your Home page wufoo form",
+                        "title": "Screenshot of the Conrex custom site Sell Us Your Home page wufoo form",
+                        "caption": "This is a Wufoo form that I built and styled for Conrex for the custom Sell Us Your Home page I built."
                     }
                 ],
             "type": "professional"
@@ -55,11 +109,47 @@ var projects =
             "photos":
                 [
                     {
-                        "url": "savannah-thumbnail.png",
-                        "alt": "",
-                        "title": "",
-                        "caption": "This is a test caption"
-                    }
+                        "url": "/savannah/specials.png",
+                        "alt": "Savannah Specials Drawer Screenshot",
+                        "title": "Savannah Theme Specials Drawer",
+                        "caption": "This is the specials drawer I built for the Savannah theme. If specials content is entered in the CMS this drawer will show. Clicking on the specials button will toggle opening and closing the drawer. Clients also have the option to trigger a modal instead of a specials drawer."
+                    },
+                    {
+                        "url": "/savannah/growing-image.png",
+                        "alt": "Savannah Home Image Screenshot Compilation",
+                        "title": "Savannah Home Image Animation",
+                        "caption": "This image in the home section of the Savannah theme grows as you scroll down the page and reach the amenities section."
+                    },
+                    {
+                        "url": "/savannah/floorplans.png",
+                        "alt": "Savannah Theme Floorplans Widget Screenshot",
+                        "title": "Savannah Floorplans Widget",
+                        "caption": "This is a custom layout that I built for our floorplans widget. The widget uses a series of html templates to build out each section and common classes for adding functionality."
+                    },
+                    {
+                        "url": "/savannah/amenities.png",
+                        "alt": "Savannah Theme Amenities Page Screenshot",
+                        "title": "Savannah Theme Amenities Page",
+                        "caption": "This is the Amenities page I built out for the Savannah widget. The amenities are displayed using a widget that I built a custom layout for."
+                    },
+                    {
+                        "url": "/savannah/neighborhood.png",
+                        "alt": "Savannah Theme Neighborhood Page Widget Screenshot",
+                        "title": "Savannah Theme Neighborhood Widget",
+                        "caption": "This is our neighborhood with layouts widget that I built a custom layout for, the categories side panel is where I did most of my customization."
+                    },
+                    {
+                        "url": "/savannah/contact.png",
+                        "alt": "Savannah Theme Contact Page Screenshot",
+                        "title": "Savannah Theme Contact Form",
+                        "caption": "This is the contact form section of the Savannah theme that I styled. The form is a widget used across many of our themes."
+                    },
+                    {
+                        "url": "/savannah/lifestyle.png",
+                        "alt": "Savannah Lifestyle page carousel Screenshot",
+                        "title": "Savannah Lifestyle Page Carousel",
+                        "caption": "This is a carousel I built for one of our ad-hoc page options for the Savannah theme. Customers can fill in page items in the CMS to populate this carousel with up to 3 different items."
+                    },
                 ],
             "type": "professional"
         },
@@ -76,10 +166,28 @@ var projects =
             "photos":
                 [
                     {
-                        "url": "gaslamp-thumbnail.png",
-                        "alt": "Gaslamp Theme Thumbnail",
-                        "title": "Gaslamp Theme Thumbnail",
-                        "caption": "This is a test caption"
+                        "url": "/gaslamp/gaslamp-specials.png",
+                        "alt": "Gaslamp Specials Drawer Screenshot",
+                        "title": "Gaslamp Theme Specials Drawer",
+                        "caption": "This is the specials drawer I built for the Gaslamp theme. If specials content is entered in the CMS this drawer will show. Clicking on the specials button will toggle opening and closing the drawer."
+                    },
+                    {
+                        "url": "/gaslamp/gaslamp-neighborhood.png",
+                        "alt": "Gaslamp Theme Neighborhood Page Widget Screenshot",
+                        "title": "Gaslamp Theme Neighborhood Widget",
+                        "caption": "This is one of our standard neighborhood map widgets that I styled for this theme. I modified the style to only show the category text on hover, among some other smaller changes."
+                    },
+                    {
+                        "url": "/gaslamp/gaslamp-contact.png",
+                        "alt": "Gaslamp Theme Contact Page Screenshot",
+                        "title": "Gaslamp Theme Contact Form",
+                        "caption": "This is the contact form section of the Gaslamp theme that I styled. The form is a widget used across many of our themes."
+                    },
+                    {
+                        "url": "/gaslamp/gaslamp-mobile-nav.png",
+                        "alt": "Gaslamp Theme Mobile Navigation Screenshot",
+                        "title": "Gaslamp Theme Mobile Navigation",
+                        "caption": "This is the mobile navigation that I built for the Gaslamp theme."
                     }
                 ],
             "type": "professional"
